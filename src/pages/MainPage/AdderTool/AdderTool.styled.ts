14c89b913e410082ef014ead185dcbd6d0865a9a
hyejun <hyejunkkim228@gmail.com>
2025-04-30 21:41:38 +0900

    feat: AdderTool 구현

--- src/pages/MainPage/AdderTool/AdderTool.styled.ts
+++ src/pages/MainPage/AdderTool/AdderTool.styled.ts
@@ -15,11 +15,62 @@ export const Title = styled.h2`
 
 export const MainContainer = styled.div`
   display: flex;
+  justify-content: space-between;
   padding: 12px 20px;
-  flex-direction: column;
   gap: 8px;
   border-radius: 12px;
   border: 1px solid rgba(0, 0, 0, 0.08);
   background: #fff;
   box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.04);
+`;
+
+export const ProcessorNameInputWrapper = styled.div`
+  display: flex;
+  width: 372px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProcessInputWrapper = styled.div`
+  display: flex;
+  min-width: 819px;
+  gap: 24px;
+`;
+
+export const InputTitle = styled.div`
+  color: #222;
+  white-space: nowrap;
+
+  font-family: 'SUIT Variable';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  margin-right: 8px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  min-width: 112px;
+  height: 36px;
+  padding: 2px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  background: #202020;
+  color: #f9f9f9;
+  white-space: nowrap;
+  font-family: 'SUIT Variable';
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+
+  margin-left: 24px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  max-width: 223px;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
 `;
--- src/pages/MainPage/AdderTool/AdderTool.tsx
+++ src/pages/MainPage/AdderTool/AdderTool.tsx
@@ -1,10 +1,59 @@
+import { useState } from 'react';
+
 import * as S from './AdderTool.styled';
 
+import TextField from '@/components/common/TextField';
+
 function AdderTool() {
+  const [processorValue, setProcessorValue] = useState('');
+  const [processNameValue, setProcessNameValue] = useState('');
+  const [processArrivalValue, setProcessArrivalValue] = useState('');
+  const [processBurstValue, setProcessBurstValue] = useState('');
   return (
     <S.Container>
       <S.Title>Adder Tool</S.Title>
-      <S.MainContainer>asd</S.MainContainer>
+      <S.MainContainer>
+        <S.ProcessorNameInputWrapper>
+          <S.InputTitle>Processor Name :</S.InputTitle>
+          <TextField
+            value={processorValue}
+            onChange={(e) => setProcessorValue(e.currentTarget.value)}
+            name="Processor Name"
+            required
+          />
+          <S.Button type="button">Add Processor</S.Button>
+        </S.ProcessorNameInputWrapper>
+        <S.ProcessInputWrapper>
+          <S.InputWrapper>
+            <S.InputTitle>Process Name :</S.InputTitle>
+            <TextField
+              value={processNameValue}
+              onChange={(e) => setProcessNameValue(e.currentTarget.value)}
+              name="Process Name"
+              required
+            />
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.InputTitle>Arrival Time :</S.InputTitle>
+            <TextField
+              value={processArrivalValue}
+              onChange={(e) => setProcessArrivalValue(e.currentTarget.value)}
+              name="Arrival Time"
+              required
+            />
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.InputTitle>Burst Time :</S.InputTitle>
+            <TextField
+              value={processBurstValue}
+              onChange={(e) => setProcessBurstValue(e.currentTarget.value)}
+              name="Burst Time"
+              required
+            />
+          </S.InputWrapper>
+          <S.Button type="button">Add Process</S.Button>
+        </S.ProcessInputWrapper>
+      </S.MainContainer>
     </S.Container>
   );
 }
