57d1d09d1c839afb1fc88ae692357b8fc94082be
hyejun <hyejunkkim228@gmail.com>
2025-05-07 03:18:00 +0900

    feat: 에러 처리 및 프로세스 삭제 로직 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.tsx';
-import Toast from './components/common/Toast/Toast.tsx';
 import GlobalStyles from './styles/global/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <>
     <GlobalStyles />
     <App />
-    <Toast />
   </>
 );
