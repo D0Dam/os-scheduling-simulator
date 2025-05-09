db4a7d011f4eda133f05c466afc1318ca84b9adc
박성빈 <psb106305@gmail.com>
2025-05-09 19:58:06 +0900

    refactor: 알고리즘 코드 정리

--- src/scheduler/CustomAlgorithm.ts
+++ src/scheduler/CustomAlgorithm.ts
@@ -1,35 +1,35 @@
-import { Core, PCore, Process } from '@/models';
+import { Core, PCore } from '@/models';
 import { Scheduler } from '@/scheduler';
 
 export class CustomAlgorithm extends Scheduler {
-  #pCores: Core[] = [];
+  pCores: Core[] = [];
 
   get result() {
-    this.#pCores = this.cores.filter((core) => core instanceof PCore);
+    this.pCores = this.cores.filter((core) => core instanceof PCore);
     return super.result;
   }
 
-  protected assignProcess(): void {
+  protected assignProcess() {
     this.cores.forEach((core) => {
       if (!core.hasProcess) {
-        this.#sort(core);
+        this.sort(core);
         core.setProcess(this.readyQueue.shift(), this.time);
       }
     });
   }
 
-  protected releaseProcess(): void {
+  protected releaseProcess() {
     this.cores.forEach((core) => {
       if (core.process?.isEnd()) {
         this.endQueue.push(core.releaseProcess());
-      } else if (core.hasProcess && this.#canPreempt(core)) {
+      } else if (core.hasProcess && this.canPreempt(core)) {
         this.readyQueue.push(core.releaseProcess());
       }
     });
   }
 
-  #sort(core: Core): void {
-    this.readyQueue.sort((a: Process, b: Process) => {
+  sort(core: Core) {
+    this.readyQueue.sort((a, b) => {
       const aIsSmallerThanWps = a.remainProgress < core.wps;
       const bIsSmallerThanWps = b.remainProgress < core.wps;
 
@@ -46,12 +46,12 @@ export class CustomAlgorithm extends Scheduler {
     });
   }
 
-  #canPreempt(core: Core): boolean {
-    this.#sort(core);
+  canPreempt(core: Core) {
+    this.sort(core);
     if (core.process && core.process.remainProgress < core.wps) {
       return true;
     }
-    if (!(core instanceof PCore) && this.#pCores.some((pCore) => !pCore.hasProcess)) {
+    if (!(core instanceof PCore) && this.pCores.some((pCore) => !pCore.hasProcess)) {
       return true;
     }
     return !!(this.readyQueue.length && this.readyQueue[0].remainProgress === core.wps);
